f45f57ca0d280663f164aca1cd6b17fce2fab862
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-15 19:22:45 +0800

    feat: add all pdf resources from pypdf suite

--- tests/test_password_hashes.py
+++ tests/test_password_hashes.py
@@ -1,5 +1,5 @@
 from conftest import unlock_pdf
-from pytest import raises, mark
+from pytest import mark, raises
 
 
 def test_wrong_password():
@@ -9,17 +9,24 @@ def test_wrong_password():
 
 
 @mark.parametrize(
-    ("pdf_name"),
+    ("pdf_name", "password"),
     [
         # PDFs borrowed from https://github.com/py-pdf/pypdf/tree/8a4adac/resources/encryption
-        ("r2-user-password.pdf"),
-        ("r3-user-password.pdf"),
-        ("r4-user-password.pdf"),
-        ("r4-aes-user-password.pdf"),
-        ("r5-user-password.pdf"),
-        ("r6-user-password.pdf"),
+        ("r2-empty-password.pdf", ""),
+        ("r2-owner-password.pdf", ""),
+        ("r2-user-password.pdf", "asdfzxcv"),
+        ("r3-empty-password.pdf", ""),
+        ("r3-user-password.pdf", "asdfzxcv"),
+        ("r4-aes-user-password.pdf", "asdfzxcv"),
+        ("r4-owner-password.pdf", ""),
+        ("r4-user-password.pdf", "asdfzxcv"),
+        ("r5-empty-password.pdf", ""),
+        ("r5-user-password.pdf", "asdfzxcv"),
+        ("r6-both-passwords.pdf", "foo"),
+        ("r6-empty-password.pdf", ""),
+        ("r6-owner-password.pdf", ""),
+        ("r6-user-password.pdf", "asdfzxcv"),
     ],
 )
-def test_encryption(pdf_name):
-    password = unlock_pdf(f"tests/pdf/{pdf_name}", "asdfzxcv")
-    assert password == "asdfzxcv"
+def test_encryption(pdf_name, password):
+    unlock_pdf(f"tests/pdf/{pdf_name}", password)

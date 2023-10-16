6ea3c80101c22e57ebbb7dd892e29a65f2720f46
Benjamin Dornel <benjamindornel@gmail.com>
2023-10-16 23:53:31 +0800

    refactor: remove redundant classes/functions & add docstrings

--- pdf2john.py
+++ pdf2john.py
@@ -3,71 +3,56 @@
 import logging
 import os
 import sys
-from enum import Enum
 
-from pyhanko.pdf_utils.crypt import SecurityHandlerVersion
 from pyhanko.pdf_utils.reader import PdfFileReader
 
 logger = logging.getLogger(__name__)
 
 
-class SecurityRevision(Enum):
-    RC4_BASIC = (2, 32)
-    RC4_EXTENDED = (3, 32)
-    RC4_OR_AES128 = (4, 32)
-    AES_256_R5 = (5, 48)
-    AES256 = (6, 48)
+class SecurityRevision:
+    """Represents Standard Security Handler Revisions
+    and the corresponding key length for the /O and /U entries
 
-    @property
-    def revision(self):
-        return self.value[0]
+    In Revision 5, the /O and /U entries were extended to 48 bytes,
+    with three logical parts -- a 32 byte verification hash,
+    an 8 byte validation salt, and an 8 byte key salt."""
 
-    @property
-    def key_length(self):
-        return self.value[1]
+    revisions = {
+        2: 32,  # RC4_BASIC
+        3: 32,  # RC4_EXTENDED
+        4: 32,  # RC4_OR_AES128
+        5: 48,  # AES_R5_256
+        6: 48,  # AES_256
+    }
 
     @classmethod
     def get_key_length(cls, revision):
-        for item in cls:
-            if item.revision == revision:
-                return item.key_length
-        logger.warning(
-            "No explicit key length for revision %s, using default length of 48",
-            revision,
-        )
-        return 48
-
-
-class Entries(Enum):
-    U = "udata"
-    O = "odata"
-    UE = "ueseed"
-    OE = "oeseed"
-
-
-class EncryptionDictionary:
-    def __init__(self, pdf: PdfFileReader):
-        encryption_dict = pdf._get_encryption_params()
-
-        if not encryption_dict:
-            raise RuntimeError("File not encrypted")
-
-        self.algorithm: int = encryption_dict.get(
-            "/V", SecurityHandlerVersion.RC4_40.value
-        )
-        self.key_length: int = encryption_dict.get("/Length", 40)
-        self.permissions: int = encryption_dict["/P"]
-        self.revision: int = encryption_dict["/R"]
+        """
+        Get the key length for a given revision,
+        defaults to 48 if no revision is specified.
+        """
+        return cls.revisions.get(revision, 48)
 
 
 class PdfHashExtractor:
+    """
+    Extracts hash and encryption information from a PDF file
+    """
+
     def __init__(self, file_name):
         self.file_name = file_name
 
         with open(file_name, "rb") as doc:
             self.pdf = PdfFileReader(doc, strict=False)
-            self.encryption = EncryptionDictionary(self.pdf)
-            self.security_handler = self.pdf.security_handler
+            encrypt_dict = self.pdf._get_encryption_params()
+
+            if not encrypt_dict:
+                raise RuntimeError("File not encrypted")
+
+            self.algorithm: int = encrypt_dict.get("/V")
+            self.length: int = encrypt_dict.get("/Length", 40)
+            self.permissions: int = encrypt_dict["/P"]
+            self.revision: int = encrypt_dict["/R"]
 
     @property
     def document_id(self) -> bytes:
@@ -75,21 +60,23 @@ class PdfHashExtractor:
 
     @property
     def encrypt_metadata(self) -> str:
-        return str(int(self.security_handler.encrypt_metadata))
+        """
+        Get a string representation of whether metadata is encrypted.
 
-    @property
-    def entries(self) -> dict:
-        return {
-            enum.name: getattr(self.security_handler, enum.value) for enum in Entries
-        }
+        Returns "1" if metadata is encrypted, "0" otherwise.
+        """
+        return str(int(self.pdf.security_handler.encrypt_metadata))
 
     def parse(self) -> str:
-        passwords = self.get_passwords(self.entries, self.encryption.revision)
+        """
+        Parse PDF encryption information into a formatted string for John
+        """
+        passwords = self.get_passwords()
         fields = [
-            f"$pdf${self.encryption.algorithm}",
-            self.encryption.revision,
-            self.encryption.key_length,
-            self.encryption.permissions,
+            f"$pdf${self.algorithm}",
+            self.revision,
+            self.length,
+            self.permissions,
             self.encrypt_metadata,
             len(self.document_id),
             self.document_id.hex(),
@@ -97,14 +84,18 @@ class PdfHashExtractor:
         ]
         return "*".join(map(str, fields))
 
-    @staticmethod
-    def get_passwords(entries: dict[str, bytes], revision: int) -> str:
+    def get_passwords(self) -> str:
+        """
+        Creates a string consisting of the hexidecimal string of the
+        /U, /O, /UE and /OE entries and their corresponding byte string length
+        """
         passwords = []
+        keys = ("udata", "odata", "oeseed", "ueseed")
+        max_key_length = SecurityRevision.get_key_length(self.revision)
 
-        for key, data in entries.items():
-            if data and key in list(Entries.__members__):
-                max_length = SecurityRevision.get_key_length(revision)
-                data = data[:max_length]
+        for key in keys:
+            if data := getattr(self.pdf.security_handler, key):
+                data: bytes = data[:max_key_length]
                 passwords.extend([str(len(data)), data.hex()])
 
         return "*".join(passwords)
